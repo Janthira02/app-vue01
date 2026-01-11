8fad025467ae3f96501fe9557e865ffb99755bb7
Janthira02 <lionny2545@gmail.com>
2026-01-11 16:44:39 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template >
+    <div>
+        <h1>Contact to Janthira Phosarach</h1>
+        <h3>Tel.085-517-2774</h3>
+        <img alt="Vue logo" src="../assets/me.jpg">
+    </div>
+</template>
+
