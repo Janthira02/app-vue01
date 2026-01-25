069817a4bfc064cd7fe34c1f707dd6b2c8048ac0
ชืJanthira02 <lionny2545@gmail.com>
2026-01-25 19:08:29 +0700

    update250126

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,62 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/customer',
-    name: 'customer',
-    component: () => import( '../views/Customer.vue')
+    path: "/customer",
+    name: "customer",
+    component: () => import("../views/Customer.vue"),
   },
   {
-    path: '/contact',
-    name: '/contact',
-    component: () => import( '../views//Contact.vue')
+    path: "/contact",
+    name: "/contact",
+    component: () => import("../views//Contact.vue"),
   },
   {
-    path: '/type',
-    name: '/type',
-    component: () => import( '../views//Type.vue')
+    path: "/type",
+    name: "/type",
+    component: () => import("../views//Type.vue"),
   },
   {
-    path: '/employees',
-    name: '/employees',
-    component: () => import( '../views//Employees.vue')
+    path: "/employees",
+    name: "/employees",
+    component: () => import("../views//Employees.vue"),
+  },
+  {
+    path: "/add_customer",
+    name: "/add_customer",
+    component: () => import("../views//Add_customer.vue"),
+  },
+  {
+    path: "/add_employee",
+    name: "/add_employee",
+    component: () => import("../views//Add_employee.vue"),
+  },
+  {
+    path: "/product",
+    name: "/product",
+    component: () => import("../views//Product.vue"),
+  },
+  {
+    path: "/product_api",
+    name: "/product_api",
+    component: () => import("../views//Product_api.vue"),
+  },
+  {
+    path: "/show_product",
+    name: "/show_product",
+    component: () => import("../views//Show_product.vue"),
   }
-
-
-
-
-
-
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
